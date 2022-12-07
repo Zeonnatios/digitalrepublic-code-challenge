cecedda9586ce8f480b7a8861dcc40adfc0c6d6d
Matheus Antonio <matheusantonio232@gmail.com>
2022-12-06 21:43:04 -0300

    feat: adicionado middleware handler para erros

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const { errorHandler } = require('./src/middlewares/errorHandler');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
